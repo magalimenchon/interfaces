ad757529e40877f44dc82d5721271ba021cf1c1b
Magalí Menchón <magalimenchon@gmail.com>
2021-09-05 23:33:57 -0300

    Se agregó input de selección de color

---
+++ Anexos/Filtros/pencil.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+
+    let colorInput = document.querySelector('#color');
+    let hexInput = document.querySelector('#hex');
+
+    colorInput.addEventListener('input', () => {
+        let color = colorInput.value;
+        hexInput.value = color;
+        document.body.style.backgroundColor = color;
+    });
+});
+
+function IniciarPagina() {
+
+}
