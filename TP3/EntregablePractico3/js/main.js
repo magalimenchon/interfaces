54d25475118fb2771bbc779f82778b8c3833a0ac
Lucianosc <scaminaci.luciano@gmail.com>
2021-10-24 18:56:21 -0300

    jump debugged

--- TP3/EntregablePractico3/js/main.js
+++ TP3/EntregablePractico3/js/main.js
@@ -4,29 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const avatar = document.querySelector('#avatar');
 
-    /*document.addEventListener("keydown", (event) =>{
-        //console.log("saltando", event.key);
-        if(event.key.toLowerCase() == "w"){
-            console.log("saltando", event.key);
-            avatar.classList.remove("running");
-            avatar.classList.add("jumping");
-            console.log(avatar);
-        }
-    });*/
-/*
-    document.addEventListener("keyup", (event) =>{
-        console.log("termino animacion", event.key);
-        if(event.key.toLowerCase() == "w"){
-            console.log(avatar);
-            avatar.classList.remove("jumping");
-            avatar.classList.add("running");
-        }
-    });*/
-    /*avatar.addEventListener("animationend", () =>{
-        avatar.classList.remove("jumping");
-        avatar.classList.add("running");
-    })*/
-
     document.addEventListener('keyup', action);
 
     avatar.addEventListener('animationend', restartAnimation);
