06424f21d77112bf6bb1c096f659aa47c3fad4e4
Magalí Menchón <magalimenchon@gmail.com>
2021-10-25 01:43:20 -0300

    Se agrega interfaz de inicio de juego

--- TP3/EntregablePractico3/js/main.js
+++ TP3/EntregablePractico3/js/main.js
@@ -2,12 +2,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
     "use strict";
 
+    /**
+     *  --------------- CONSTANTES --------------- 
+     */
     const avatar = document.querySelector('#avatar');
+    const buttonPlay = document.querySelector('#button-play-js');
+    const containerMenu = document.querySelector('#menu-js');
+    const containerGame = document.querySelector('#game-js');
+    /**
+     * --------------- EVENTS LISTENERS --------------- 
+     */
 
+    /**
+     * MENU
+     */
+    buttonPlay.addEventListener('click', startGame);
+
+    /**
+     * GAME
+     */
     document.addEventListener('keyup', action);
 
     avatar.addEventListener('animationend', restartAnimation);
 
+    /**
+     * --------------- FUNCTIONS --------------- 
+     */
+
+    /**
+     * MENU
+     */
+    function startGame(){
+        changeMenuForGame();
+        //quizas se deban agregar mas cuestiones del juego
+        //como el temporizador, la seleccion del avatar y fondo, etc
+    }
+
+    function changeMenuForGame(){
+        containerMenu.classList.remove("general-info");
+        containerMenu.classList.add("hidden");
+        //lo siguiente seria crear una instancia de un nuevo juego:
+        //-----------
+        //Chequear que toma el evento de la tecla w igual, aunque no este el juego renderizado
+        //-------
+        containerGame.classList.remove("hidden");
+        containerGame.classList.add("game");
+    }
+
+    /**
+     * GAME
+     */
     function action(event){
         if(event.key.toLowerCase() == "w"){
             jump();
