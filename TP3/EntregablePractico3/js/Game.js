d9a9d7f790c0fb54806bce298845939958876d93
Magalí Menchón <magalimenchon@gmail.com>
2021-10-26 16:34:42 -0300

    Se agrega avance con renderizado obstaculos

--- TP3/EntregablePractico3/js/Game.js
+++ TP3/EntregablePractico3/js/Game.js
@@ -5,7 +5,7 @@ class Game {
         this.end = false;
         this.avatar = new Avatar();
         this.DOMGame = document.querySelector('#game-js');
-         
+
     }
 
     startGameLoop() {
@@ -13,33 +13,72 @@ class Game {
         console.log("empezo el juego");
         this.renderStartGame();
 
-        setTimeout(() => { 
-            let obs = new Obstacle();
-            obs.init();
-            
-        }, 2000);
+        //sacando el hidden del bush
+        //document.querySelector("#draw-layer").lastChild.previousSibling.classList.remove("hidden");
+        /*const newDiv = document.createElement("div");
+        newDiv.classList.add('asset');
+        newDiv.classList.add('bush');
+        document.querySelector("#draw-layer").appendChild(newDiv);*/
 
-        let gameLoop = setInterval(function () {
+        /* setTimeout(() => { 
+             let obs = new Obstacle();
+             obs.init();
+             
+         }, 2000);*/
+
+
+        let gameLoop = setInterval(() => {
             // processInput();
             // updateState();
             // draw();
-            
-
+            /*this.createObstacles();
+            setTimeout(() => {this.createObstacles()}, Math.random() * 50000);*/
             //quizas se deban agregar mas cuestiones del juego
             //como el temporizador, la seleccion del avatar y fondo, etc
 
-            setTimeout(() => { this.end = true }, 5000);
-
+            setTimeout(() => { this.end = true }, 50000);
+            ///SOLUCION 1
+           let randomTime = 50;
+           let max = 50;
+           let min = 10;
+            let generateObstaclesLoop = setTimeout(() => {
+                this.createObstacles();
+              
+               randomTime += Math.floor(Math.random() * (max - min)) + min;
+                
+            }, randomTime);
             
+           /////////////////////////
+           //SOLUCION 2
+
+           /*let randomTime = Math.random() * (8000 - 1000) + 1000;
+           this.createObstacles();
+           let timeId = setInterval(function(){
+               if(this.end){
+                   clearInterval(timeId);
+               }
+           }, 20);
+           setTimeout(this.createObstacles(), randomTime);
+           this.createObstacles();*/
+           ///////////////////
             if (this.end) {
+                //clearInterval(generateObstaclesLoop);
                 clearInterval(gameLoop);
                 console.log("termino el juego");
             }
-        }, 50);
+        }, 2500);
         // endGame();
     }
 
-    renderStartGame(){
+    createObstacles() {
+        const max = 3;
+        const min = 1;
+        const type = Math.floor(Math.random() * (max - min)) + min;
+        let obs = new Obstacle();
+        obs.init(type);
+    }
+
+    renderStartGame() {
         this.renderRemoveMenu();
         //Se agrega el avatar
         this.renderAvatar();
@@ -54,28 +93,26 @@ class Game {
         this.renderRemoveFlyerPrincipal();
     }
 
-    renderRemoveFlyerPrincipal(){
+    renderRemoveFlyerPrincipal() {
         const flyerPrincipal = this.DOMGame.lastChild.previousSibling;
         console.log(flyerPrincipal);
         this.DOMGame.removeChild(flyerPrincipal);
     }
-    renderRemoveFlyerRunner(){
+    renderRemoveFlyerRunner() {
         const flyerRunner = this.DOMGame.firstChild.nextSibling;
         flyerRunner.classList.remove("information");
         flyerRunner.removeChild(flyerRunner.firstChild.nextSibling);
     }
 
-    renderAvatar(){
+    renderAvatar() {
         const avatar = document.querySelector('#avatar');
         avatar.classList.remove("hidden");
-        console.log(avatar);
     }
 
     processInput(event) {
         if (event.key.toLowerCase() == "w") {
             this.avatar.jump();
         }
-
     }
 
 
--- TP3/EntregablePractico3/js/Obstacle.js
+++ TP3/EntregablePractico3/js/Obstacle.js
@@ -1,12 +1,28 @@
 class Obstacle {
 
     constructor(){
+        this.id = Math.random() * 500000;
+        this.idDOM;
     }
 
-    init(){
+    init(type){
         const newDiv = document.createElement("div");
         newDiv.classList.add('asset');
-        newDiv.classList.add('cactus2');
+        if(type == 1){
+            newDiv.classList.add('cactus2');
+        }
+        else
+            newDiv.classList.add('bush');
+        newDiv.setAttribute("id", this.id);
         document.querySelector("#draw-layer").appendChild(newDiv);
+        this.idDOM = document.getElementById(this.id);
+        this.idDOM.addEventListener('animationend', () => {
+            this.destroyObstacle();
+        });
+    }
+
+    destroyObstacle() {
+        document.querySelector("#draw-layer").removeChild(this.idDOM);
+        // this.DOMavatar.style.animationPlayState = "paused";
     }
 }
