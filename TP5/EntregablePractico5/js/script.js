3ce129bdd86b8131da9d45c79ef07735b575f9fa
Lucianosc <scaminaci.luciano@gmail.com>
2021-12-01 13:58:20 -0300

    added parrallax

--- TP5/EntregablePractico5/js/script.js
+++ TP5/EntregablePractico5/js/script.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // document.getElementsByClassName("");
-
+    document.addEventListener("mousemove", parallax);
+    function parallax(e){
+        
+        document.querySelectorAll(".parrallax").forEach(function(move){
+            let moving_value = move.getAttribute("data-value");
+            let x = (((e.clientX - (window.innerWidth /2)) * moving_value) /500) ;
+            let y = (((e.clientY - (window.innerHeight /2)) * moving_value) /500) ;
+            move.style.transform = "translateX(" + -x + "px) translateY(" + -y + "px)";
+        });
+    }
 });
